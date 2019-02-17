93f60f03228ddcb46b22481ac404b829069fc83a
emmaife <emma.s.ife@gmail.com>
2019-02-17 18:05:32 -0500

    removed user cards alter migrations

--- db/migrate/20190211204339_add_user_to_user_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToUserCards < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :user_cards, :user, foreign_key: true
-  end
-end
--- db/migrate/20190211204404_add_card_to_user_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddCardToUserCards < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :user_cards, :card, foreign_key: true
-  end
-end
